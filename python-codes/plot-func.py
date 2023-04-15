f22840182c5909c8b5126d311c0f1a002f908594
Ritali Ghosh <ritsg11@gmail.com>
2023-04-15 11:33:46 +0530

    [ADD] added python file <plot-func.py>

---
+++ python-codes/plot-func.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+x = np.linspace(-30, 30, 200)
+eps = 1e-5
+plt.plot(x, (np.sin(x)+eps)/(x+eps))
+plt.ylabel("sin(x)/x")
+plt.xlabel("x")
+plt.grid(ls=":")
+plt.show()
